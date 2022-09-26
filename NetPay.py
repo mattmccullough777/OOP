32b49e2142aa06634d7cc79680f0f6637b47830b
Matthew <mattmccullough777@gmail.com>
2022-09-26 13:23:07 -0500

    update1

--- NetPay.py
+++ NetPay.py
@@ -6,64 +6,36 @@ import PayrollDeductionClass as t
 
 employee = l.Employee("Jimmy Smith","58475","Information Systems","Developer","$6,800.00")
 
-#Option 1
-#print(" Name       ID Number       Department        Job Title       Monthly Salary      \n",
-#employee.get_name(),employee.get_IDnumber(),employee.get_department(),employee.get_jobtitle(),employee.get_salary())
-
-#Option 2
 employee_table = [["Name","ID Number","Department","Job Title","Monthly Salary"],[employee.get_name(),employee.get_IDnumber(),employee.get_department(),employee.get_jobtitle(),employee.get_salary()]]
 print(tabulate(employee_table))
 
 
+payroll_1 = t.PayrollTransactions("food court","8/14/2022",22.50,"39119") 
 
-#########################################
+payroll_2 = t.PayrollTransactions("gift contribution","8/12/2022",25.00,"58475")
+
+payroll_3 = t.PayrollTransactions("food court","8/17/2022",15.25,"21547")
+
+payroll_4 = t.PayrollTransactions("vending machine","8/22/2022",3.00,"58475")
+
+payroll_5 = t.PayrollTransactions("vanding machine","8/5/2022",2.75,"58475")
+
+payroll_table = [["Description","Date","Charge","EmployeeID"],[payroll_1.get_description(),payroll_1.get_date(),payroll_1.get_charge(),payroll_1.get_employeeID()],[payroll_2.get_description(),payroll_2.get_date(),payroll_2.get_charge(),payroll_2.get_employeeID()],[payroll_3.get_description(),payroll_3.get_date(),payroll_3.get_charge(),payroll_3.get_employeeID()],[payroll_4.get_description(),payroll_4.get_date(),payroll_4.get_charge(),payroll_4.get_employeeID()],[payroll_5.get_description(),payroll_5.get_date(),payroll_5.get_charge(),payroll_5.get_employeeID()]]
 
 
-transactions = float(input("How many transactions do you have? "))
+print(tabulate(payroll_table))
 
-count = 0
-
-#description_table=[]
-#date_table=[]
-charge_table=[]
-#employeeID_table=[]
-
-
-while count < transactions:
-    description = input("What is the description of the transaction? ")
-    date = input("What date did the transaction happen? ")
-    charge = float(input("What was the transaction charge? "))
-    employeeID = input("What is the employee ID? ")
-    #description_table.append(description)
-    #date_table.append(date)
-    charge_table.append(charge)
-    #employeeID_table.append(employeeID)
-    count +=1
+if employee.get_IDnumber() == payroll_1.get_employeeID() or payroll_2.get_employeeID() or payroll_3.get_employeeID() or payroll_4.get_employeeID() or payroll_5.get_employeeID():
+    charge_table=[payroll_2.get_charge(),payroll_4.get_charge(),payroll_5.get_charge()]
+    Total = sum(charge_table)
 
 
 
-payroll = t.PayrollTransactions(description,date,charge,employeeID)
-transaction_table = [["Description","Date","Charge","EmployeeID"],[payroll.get_description(),payroll.get_date(),payroll.get_charge(),payroll.get_employeeID()]]
-
-
-
-print(tabulate(transaction_table))
-
-Total = sum(charge_table)
-
-
-
-#####################################################
-
-
-print("Employee Pay")
+print("*** Employee Pay ***")
 print("Name: ", employee.get_name())
 print("ID Number: ",employee.get_IDnumber())
 print("Department: ",employee.get_department())
 print("Gross Pay: ",employee.get_salary())
 salary = (employee.get_salary().replace("$","").replace(",",""))
 salary = float(salary)
-print("Net Pay: $", salary - Total)
-
-
-
+print("Net Pay: $", "{:,}".format(salary - Total))
